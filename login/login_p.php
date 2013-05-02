bbeddcaf67cc3285f9b42e18fa6d586445012be1
Martin Høgh <mh@mapcentia.com>
2013-05-02 11:11:48 +0200

    user

--- login/login_p.php
+++ login/login_p.php
@@ -8,12 +8,13 @@ include '../html_header.php';
             <?php
             // Check if user is logged in - and redirect if this is not the case
             if (!$_SESSION['auth'] || !$_SESSION['screen_name']) {
-                die("<sccript>window.location='{$userHostName}/user/login'</script>");
+                die("<script>window.location='{$userHostName}/user/login'</script>");
             }
             ($_SESSION['zone']) ? $prefix = $_SESSION['zone'] . "." : $prefix = "";
             $checkDb = json_decode(file_get_contents("http://{$prefix}{$domain}/controller/databases/postgres/doesdbexist/{$_SESSION['screen_name']}"));
             if ($checkDb->success) {
                 echo "<a href='http://{$prefix}{$domain}/store/{$_SESSION['screen_name']}' id='btn-admin' class='btn btn-large btn-info' title='' data-placement='top' data-content='Click here to start the geocloud administration.'>Start admin</a>";
+                file_get_contents("http://{$prefix}{$domain}/controller/mapfile/{$_SESSION['screen_name']}/public/create");
             } else {
                 echo "<a href='http://{$prefix}{$domain}/user/createstore' id='btn-admin' class='btn btn-large btn-info' title='' data-placement='top' data-content='Click here to create your geocloud. It may take some secs, so stay on this page.'>Create new database</a>";
             }
