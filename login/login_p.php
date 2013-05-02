8b312212939808faadfea0b2c7450ba8b097617d
Martin Høgh <mh@mapcentia.com>
2013-05-02 11:19:48 +0200

    user

--- login/login_p.php
+++ login/login_p.php
@@ -13,6 +13,7 @@ include '../html_header.php';
             ($_SESSION['zone']) ? $prefix = $_SESSION['zone'] . "." : $prefix = "";
             $checkDb = json_decode(file_get_contents("http://{$prefix}{$domain}/controller/databases/postgres/doesdbexist/{$_SESSION['screen_name']}"));
             if ($checkDb->success) {
+
                 echo "<a href='http://{$prefix}{$domain}/store/{$_SESSION['screen_name']}' id='btn-admin' class='btn btn-large btn-info' title='' data-placement='top' data-content='Click here to start the geocloud administration.'>Start admin</a>";
                 file_get_contents("http://{$prefix}{$domain}/controller/mapfile/{$_SESSION['screen_name']}/public/create");
             } else {
