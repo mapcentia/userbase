68f0bc5b0bbeec304589a59cfd1c9155bb31d3de
Martin Høgh <mh@mapcentia.com>
2013-05-27 08:56:19 +0200

    EU1

--- signup/registration_f.php
+++ signup/registration_f.php
@@ -62,7 +62,7 @@ include '../html_header.php';
                             <input type="radio" name="Zone" value="us1" checked>
                             North America </label>
                         <label class="radio">
-                            <input type="radio" name="Zone" value="eu1" disabled="true">
+                            <input type="radio" name="Zone" value="eu1">
                             Europe </label>
                         <label class="radio">
                             <input type="radio" name="Zone" value="local">
